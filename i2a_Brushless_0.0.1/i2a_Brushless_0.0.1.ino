e6acee9ab9ca9fb55406b06769fa8c00bdbadce6
iliatech <iadomyshev@gmail.com>
2023-02-07 20:22:55 +0400

    brushless with autostart

--- i2a_Brushless_0.0.1/i2a_Brushless_0.0.1.ino
+++ i2a_Brushless_0.0.1/i2a_Brushless_0.0.1.ino
@@ -1,15 +1,21 @@
 #include "Servo.h"
 
 Servo motor;
+int minMotorVal = 800; // Could not be less than 800;
+int maxMotorVal = 860; // Could be not more than 2300;
 
 void setup() {
   Serial.begin(9600);
   motor.attach(2);
+  motor.writeMicroseconds(2300);
+  delay(1000);
+  motor.writeMicroseconds(800);
+  delay(6000);
 }
 
 void loop() {
   int analogVal = analogRead(0);
-  int motorVal = constrain(map(analogVal, 0, 1023, 800, 2300), 800, 2300);
+  int motorVal = constrain(map(analogVal, 0, 1023, minMotorVal, maxMotorVal), minMotorVal, maxMotorVal);
   motor.writeMicroseconds(motorVal);
   Serial.println(motorVal);
 }
