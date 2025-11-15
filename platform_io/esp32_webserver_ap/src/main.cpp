acbb55f091bb07bd272268b02989b85780b4a208
idomyshev <iadomyshev@gmail.com>
2025-11-15 08:30:36 +0100

    answer with json

--- platform_io/esp32_webserver_ap/src/main.cpp
+++ platform_io/esp32_webserver_ap/src/main.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include <WiFi.h>
 #include <ESP32Servo.h>
+#include <ArduinoJson.h>
 
 Servo myServo;
 int servoPin = 18;
@@ -45,6 +46,9 @@ void stopAllMotors();
 // Set the ESP32 as an access point
 const char *ssid = "ESP32-AP";
 const char *password = "Angara86**"; // Password is optional
+
+JsonDocument doc;
+String jsonResponse;
 
 // Set web server port number to 80
 WiFiServer server(80);
@@ -127,19 +131,19 @@ void loop()
                 { // If the byte is a newline character
                     if (currentLine.length() == 0)
                     {
+
+               
                         // HTTP response
                         client.println("HTTP/1.1 200 OK");
                         client.println("Content-type:text/html");
                         client.println("Connection: close");
                         client.println();
 
-                        // Display the web page
-                        client.println("<!DOCTYPE html><html>");
-                        client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
-                        client.println("<title>Ilia's ESP32</title></head>");
+                        doc["ledState"] = ledState;
+                        doc["ledPin"] = ledPin;
 
-                        // Display current LED state
-                        client.println("<p>LED status is " + ledState + "</p>");
+                        serializeJson(doc, jsonResponse);
+                        client.println(jsonResponse);
 
                         // The HTTP response ends with another blank line
                         client.println();
